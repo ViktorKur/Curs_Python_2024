f9a7f82cbba8825075ed3da2236404e29eee1ff5
Viktor K <popvik13@mail.ru>
2024-11-16 03:53:10 +0300

    Dobavlen seminar_5 and dom_zadanie

--- Lek_3/Semenar_5/HW/DZ_1.py
+++ Lek_3/Semenar_5/HW/DZ_1.py
@@ -17,7 +17,30 @@
 # Значение ключа: None
 
 
-# Мой вариант решения
+# Мой вариант решения (подсмотрен)
+def search_key(key, data, depth=0): 
+  res = None 
+  if key in data: 
+      return data[key] 
+  if depth - 1: 
+      for value in data.values(): 
+          if isinstance(value, dict): 
+              res = search_key(key, value, depth - 1) 
+              if res: 
+                  return res 
+  return res 
+
+site = {'html': {'head': {'title': 'Мой сайт'},'body': {'h2': 'Здесь будет мой заголовок','div': 'Тут, наверное, какой-то блок', 'p': 'А вот здесь новый абзац'}}}
+iskom_key = input('Введите искомый ключ: ') 
+vibor = input('Хотите ввести максимальную глубину? Y/N: ').upper() 
+if vibor == 'Y': 
+    depth = int(input('Введите максимальную глубину: ')) 
+    print(f'Значение ключа: {search_key(iskom_key, site, depth)}') 
+elif vibor == 'N': 
+    print(f'Значение ключа: {search_key(iskom_key , site)}')
+else:
+    print("Неправильный параметр, нужно было ввести Y или N, попробуйте еще раз") 
+
 
 
 
